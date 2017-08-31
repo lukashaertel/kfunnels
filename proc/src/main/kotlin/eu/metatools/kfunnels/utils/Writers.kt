e421a18185b7a7db330d42963df02e9359d90088
Lukas Härtel <lukashaertel@gmail.com>
2017-08-31 10:24:07 +0200

    Clean up and add resources

--- proc/src/main/kotlin/eu/metatools/kfunnels/utils/Writers.kt
+++
@@ -1,9 +0,0 @@
-package eu.metatools.kfunnels.utils
-
-import java.io.Writer
-
-/**
- * Writes with [String.trimMargin].
- */
-fun Writer.writeTrimmed(string: String) =
-        write(string.trimMargin())
